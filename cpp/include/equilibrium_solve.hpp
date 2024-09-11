7030c1aed87ccfb974691d46dc9622483d5ee11a
Eamon Conway <conway.e@wehi.edu.au>
2024-09-11 12:43:18 +1000

    Fixed up mistakes, appear to be similar to Michaels results

--- cpp/include/equilibrium_solve.hpp
+++ cpp/include/equilibrium_solve.hpp
@@ -1,55 +1,142 @@
 #ifndef MWHITE_EQUILIBRIUM_HPP
 #define MWHITE_EQUILIBRIUM_HPP
+#include <cmath>
+#include <concepts>
 #include <span>
+#include <type_traits>
 namespace equilibrium {
 template <std::size_t offset, std::size_t stride, typename T>
 struct index_adaptor {
   T &data;
-  inline auto operator[](const std::size_t &index) -> typename T::value_type & {
+  inline auto operator[](const std::size_t &index)
+      -> std::conditional_t<
+          std::is_const_v<T>,
+          typename std::remove_reference_t<T>::const_reference,
+          typename std::remove_reference_t<T>::reference> {
     return data[offset + stride * index];
   };
 };
 
-struct Parameters {
+struct HillFuncParameters {
+  double c50;
+  double min;
+  double max;
+  double kappa;
+};
+
+struct ImmunityParameters {
+  double Pmat;
+  double u_par;
+  double u_clin;
+  double mu_M;
+  double mu_P;
+  double mu_C;
+};
+
+struct TransmissionParameters {
   double eir;
   double zeta;
   double f;
+  double rho;
   double a0;
-  double r_PCR;
   double r_LM;
   double r_D;
   double r_T;
   double r_P;
   double gamma_L;
   double mu_h;
-  double phiD;
-  double phiLM;
   double chi;
+};
+
+struct Parameters {
+  TransmissionParameters transmission_params;
+  ImmunityParameters immune_params;
+  HillFuncParameters phiLM;
+  HillFuncParameters phiD;
+  HillFuncParameters dPCR;
+};
+
+template <std::floating_point RealType>
+auto hill_func(std::floating_point auto x, std::floating_point auto y,
+               const HillFuncParameters &p) {
+  auto denom = [](auto x, auto y, auto c50, auto kappa) {
+    return 1.0 + std::pow((x + y) / c50, kappa);
+  };
+  return p.min + (p.max - p.min) / denom(x, y, p.c50, p.kappa);
+};
+
+template <std::floating_point RealType>
+auto immunity_rhs_equation([[maybe_unused]] std::floating_point auto age,
+                           const std::floating_point auto &y,
+                           const std::floating_point auto exposure,
+                           const std::floating_point auto decay_rate) {
+  return exposure - decay_rate * y;
+};
+
+template <std::floating_point RealType>
+auto all_immunity_rhs_equation(std::floating_point auto age,
+                               std::span<const RealType> y,
+                               std::span<RealType> y_out,
+                               std::floating_point auto lambda,
+                               const ImmunityParameters &immune_params) {
+  const auto &[Pmat, u_par, u_clin, mu_M, mu_P, mu_C] = immune_params;
+  // Antibody model for immunity.
+  auto exposure_parasite = lambda / (lambda * u_par + 1.0);
+  auto exposure_clinical = lambda / (lambda * u_clin + 1.0);
+  y_out[0] = immunity_rhs_equation<RealType>(age, y[0], 0.0, mu_M);
+  y_out[1] =
+      immunity_rhs_equation<RealType>(age, y[1], exposure_parasite, mu_P);
+  y_out[2] = immunity_rhs_equation<RealType>(age, y[2], 0.0, mu_M);
+  y_out[3] =
+      immunity_rhs_equation<RealType>(age, y[3], exposure_clinical, mu_C);
 };
 
 template <template <typename, std::size_t> typename Container,
           std::floating_point RealType, std::size_t N>
-auto residual_equation([[maybe_unused]] std::floating_point auto t,
-                       std::array<RealType, N> y, const Parameters &params) {
-  const auto [eir, zeta, f, a0, r_PCR, r_LM, r_D, r_T, r_P, gamma_L, mu_h, phiD,
-              phiLM, chi] = params;
+auto residual_equation(std::floating_point auto age,
+                       const Container<RealType, N> &y,
+                       const Parameters &params) {
 
   static constexpr auto K_max = N / 6 - 1;
-  auto output = Container<RealType, N>();
-  auto Sout = index_adaptor<0, 6, decltype(output)>{output};
-  auto IPCRout = index_adaptor<1, 6, decltype(output)>{output};
-  auto ILMout = index_adaptor<2, 6, decltype(output)>{output};
-  auto IDout = index_adaptor<3, 6, decltype(output)>{output};
-  auto Tout = index_adaptor<4, 6, decltype(output)>{output};
-  auto Pout = index_adaptor<5, 6, decltype(output)>{output};
-  auto S = index_adaptor<0, 6, decltype(y)>{y};
-  auto IPCR = index_adaptor<1, 6, decltype(y)>{y};
-  auto ILM = index_adaptor<2, 6, decltype(y)>{y};
-  auto ID = index_adaptor<3, 6, decltype(y)>{y};
-  auto T = index_adaptor<4, 6, decltype(y)>{y};
-  auto P = index_adaptor<5, 6, decltype(y)>{y};
+  static_assert(K_max > 0);
 
-  auto lambda = eir * zeta;
+  auto output = Container<RealType, N>();
+  auto Sout = index_adaptor<0 + 4, 6, decltype(output)>{output};
+  auto IPCRout = index_adaptor<1 + 4, 6, decltype(output)>{output};
+  auto ILMout = index_adaptor<2 + 4, 6, decltype(output)>{output};
+  auto IDout = index_adaptor<3 + 4, 6, decltype(output)>{output};
+  auto Tout = index_adaptor<4 + 4, 6, decltype(output)>{output};
+  auto Pout = index_adaptor<5 + 4, 6, decltype(output)>{output};
+
+  const auto &AP_mat = y[0];
+  const auto &AP = y[1];
+  const auto &AC_mat = y[2];
+  const auto &AC = y[3];
+
+  auto S = index_adaptor<0 + 4, 6, const Container<RealType, N>>{y};
+  auto IPCR = index_adaptor<1 + 4, 6, const Container<RealType, N>>{y};
+  auto ILM = index_adaptor<2 + 4, 6, const Container<RealType, N>>{y};
+  auto ID = index_adaptor<3 + 4, 6, const Container<RealType, N>>{y};
+  auto T = index_adaptor<4 + 4, 6, const Container<RealType, N>>{y};
+  auto P = index_adaptor<5 + 4, 6, const Container<RealType, N>>{y};
+
+  const auto &[eir, zeta, f, rho, a0, r_LM, r_D, r_T, r_P, gamma_L, mu_h, chi] =
+      params.transmission_params;
+
+  auto psi = (1.0 - rho * std::exp(-age / a0)) /
+             (1.0 - a0 * mu_h * rho / (1 + a0 * mu_h));
+  auto lambda = psi * zeta * eir;
+
+  auto immunity_in = std::span<const RealType>(y.begin(), 4);
+  auto immunity_out = std::span<RealType>(output.begin(), 4);
+
+  all_immunity_rhs_equation<RealType>(age, immunity_in, immunity_out, lambda,
+                                      params.immune_params);
+
+  const auto phiLM = hill_func<RealType>(AP, AP_mat, params.phiLM);
+  const auto phiD = hill_func<RealType>(AC, AC_mat, params.phiD);
+  const auto r_PCR = 1.0 / hill_func<RealType>(AP, AP_mat, params.dPCR);
+
   // First row.
   auto k = 0U;
   Sout[k] = -lambda * S[k] + r_PCR * IPCR[k] + r_P * P[k] +
--- main.cpp
+++ main.cpp
@@ -1,40 +1,87 @@
 #include "equilibrium_solve.hpp"
 #include "odepp/integrators/forward_euler.hpp"
 #include "odepp/ode_solve.hpp"
+#include <algorithm>
 #include <array>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <ranges>
-constexpr long double operator"" _yrs(long double years) {
+
+// Helpers to make sure that we are using the correct units.
+constexpr long double operator"" _years(long double years) {
   return years * 365.0;
 }
-
 constexpr long double operator"" _days(long double x) { return x; };
-
 int main() {
-  static constexpr auto K_max = 1;
+  static constexpr auto K_max = 20;
+  static constexpr auto Npop = 10000;
+  static constexpr auto mothers_age = 20.0_years;
+  const auto transmission_params =
+      equilibrium::TransmissionParameters{.eir = 100.0 / 365.0,
+                                          .zeta = 1.0,
+                                          .f = 1.0 / (41.0_days),
+                                          .rho = 0.85,
+                                          .a0 = 8.0_years,
+                                          .r_LM = 1.0 / (16.0_days),
+                                          .r_D = 1.0 / (5.0_days),
+                                          .r_T = 1.0 / (1.0_days),
+                                          .r_P = 1.0 / (6.0_days),
+                                          .gamma_L = 1.0 / (383.0_days),
+                                          .mu_h = 1.0 / (22.5_years),
+                                          .chi = 0.3};
 
-  auto y0 = std::array<double, 6 * (K_max + 1)>{2000.0};
-  const auto params = equilibrium::Parameters{.eir = 1.0 / (1.0_yrs),
-                                              .zeta = 1.0,
-                                              .f = 1.0 / (41.0_days),
-                                              .a0 = 8.0_yrs,
-                                              .r_PCR = 1.0 / (52.0_days),
-                                              .r_LM = 1.0 / (16.0_days),
-                                              .r_D = 1.0 / (5.0_days),
-                                              .r_T = 1.0 / (1.0_days),
-                                              .r_P = 1.0 / (6.0_days),
-                                              .gamma_L = 1.0 / (383.0_days),
-                                              .mu_h = 1.0 / (22.5_yrs),
-                                              .phiD = 0.0,
-                                              .phiLM = 0.0,
-                                              .chi = 0.0};
-  auto f = [params](auto age, const auto &y) {
-    return equilibrium::residual_equation<std::array>(age, y, params);
+  const auto params = equilibrium::Parameters{
+      .transmission_params = transmission_params,
+      .immune_params = {.Pmat = 0.31,
+                        .u_par = 42.4_days,
+                        .u_clin = 4.33_days,
+                        .mu_M = 1.0 / (49.9_days),
+                        .mu_P = 1.0 / (10.0_years),
+                        .mu_C = 1.0 / (30.0_years)},
+      .phiLM = {.c50 = 18.8, .min = 0.011, .max = 0.93, .kappa = 3.37},
+      .phiD = {.c50 = 24.5, .min = 0.006, .max = 0.96, .kappa = 5.63},
+      .dPCR = {.c50 = 9.9, .min = 10.0, .max = 52.6, .kappa = 3.82}};
+
+  auto initial_immunity = [](const auto &params) {
+    auto immunity_model = [](auto age, const auto &y, const auto &params) {
+      const auto &[eir, zeta, f, rho, a0, r_LM, r_D, r_T, r_P, gamma_L, mu_h,
+                   chi] = params.transmission_params;
+      auto psi = (1.0 - rho * std::exp(-age / a0)) /
+                 (1.0 - a0 * mu_h * rho / (1 + a0 * mu_h));
+      auto lambda = psi * zeta * eir;
+
+      auto immunity = std::array{0.0, 0.0, 0.0, 0.0};
+      all_immunity_rhs_equation<double>(age, y, immunity, lambda,
+                                        params.immune_params);
+      return immunity;
+    };
+
+    // Solve the immunity equations first to get the immunity of mothers.
+    auto [immune_age, immunity_value] = odepp::ode_solve(
+        odepp::integrator::forward_euler, immunity_model, 0.0, mothers_age,
+        1.0_days, std::array{0.0, 0.0, 0.0, 0.0}, params);
+
+    return immunity_value.back();
   };
+
+  const auto immunity_value = initial_immunity(params);
+
+  // Define the residual equation (its a templated function so has to be
+  // called somewhere to be instantiated)
+  auto f = [params](auto age, const auto &y) {
+    return equilibrium::residual_equation(age, y, params);
+  };
+
+  // Update the initial condition based upon mother immunity levels.
+  auto y0 = std::array<double, 6 * (K_max + 1) + 4>{0.0};
+  y0[0] = params.immune_params.Pmat * immunity_value[1];
+  y0[2] = params.immune_params.Pmat * immunity_value[3];
+  y0[4] = params.transmission_params.mu_h * Npop;
+
+  // Solve the ode
   auto [age_out, yout] = odepp::ode_solve(odepp::integrator::forward_euler, f,
-                                          0.0, 80.0_yrs, 1.0_days, y0);
+                                          0.0, 80.0_years, 1.0_days, y0);
 
   std::filesystem::create_directories("output");
   auto file = std::ofstream("output/data.csv");
@@ -42,25 +89,25 @@ int main() {
     throw;
   }
 
-  for (auto x : f(0.0, y0)) {
-    std::cout << x << std::endl;
-  }
-
   file << "age,state,hypnozoites,number\n";
   for (const auto &[age, state] : std::views::zip(age_out, yout)) {
+    file << age << ',' << "ap_mat,0," << state.at(0) << '\n';
+    file << age << ',' << "ap,0," << state.at(1) << '\n';
+    file << age << ',' << "ac_mat,0," << state.at(2) << '\n';
+    file << age << ',' << "ac,0," << state.at(3) << '\n';
     for (auto k = 0; k <= K_max; ++k) {
       file << age << ',';
-      file << "S," << k << ',' << state.at(6 * k) << '\n';
+      file << "S," << k << ',' << state.at(6 * k + 4) << '\n';
       file << age << ',';
-      file << "IPCR," << k << ',' << state.at(6 * k + 1) << '\n';
+      file << "IPCR," << k << ',' << state.at(6 * k + 1 + 4) << '\n';
       file << age << ',';
-      file << "ILM," << k << ',' << state.at(6 * k + 2) << '\n';
+      file << "ILM," << k << ',' << state.at(6 * k + 2 + 4) << '\n';
       file << age << ',';
-      file << "ID," << k << ',' << state.at(6 * k + 3) << '\n';
+      file << "ID," << k << ',' << state.at(6 * k + 3 + 4) << '\n';
       file << age << ',';
-      file << "T," << k << ',' << state.at(6 * k + 4) << '\n';
+      file << "T," << k << ',' << state.at(6 * k + 4 + 4) << '\n';
       file << age << ',';
-      file << "P," << k << ',' << state.at(6 * k + 5) << '\n';
+      file << "P," << k << ',' << state.at(6 * k + 5 + 4) << '\n';
     }
   }
   file.close();
